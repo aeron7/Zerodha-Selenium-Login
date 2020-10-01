b6f0b8a9838df7b5ff47aeb7537dfc0e53347eab
kaushikdalsaniya <64366428+kaushikdalsaniya@users.noreply.github.com>
2020-10-01 17:01:13 +0530

    Update zerodha.py

--- zerodha.py
+++ zerodha.py
@@ -5,30 +5,27 @@ from selenium import webdriver
 #Download ChromeDriver from https://chromedriver.chromium.org/downloads and Put on Same Folder
 chromedriver = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'chromedriver.exe')
 os.environ["webdriver.chrome.driver"] = chromedriver
+# write your User Id
+loginid = ""
+# write your Password
+password = ""
+# write your PIN
+loginpin = ""
 
-username=""
-password=""
-pin=""
 
-driver = webdriver.Chrome(chromedriver)
-driver.get("https://kite.zerodha.com")
-driver.maximize_window()
-time.sleep(5)
-
-username_input = '//*[@id="userid"]'
-driver.find_element_by_xpath(username_input).send_keys(username)
-
-password_input = '//*[@id="password"]'
-driver.find_element_by_xpath(password_input).send_keys(password)
-
-submit_input = '//*[@id="container"]/div/div/div/form/div[4]/button'
-driver.find_element_by_xpath(submit_input).click()
-time.sleep(5)
-
-#Pin Part
-driver.find_element_by_xpath('//*[@id="pin"]').send_keys(pin)
-driver.find_element_by_xpath('//*[@id="container"]/div/div/div/form/div[3]/button').click()
-time.sleep(3)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+driver = webdriver.Chrome()
+driver.get('https://kite.zerodha.com')
+def loginkite():
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,"userid"))).send_keys(loginid)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,"password"))).send_keys(password)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[@type='submit']"))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,"pin"))).send_keys(loginpin)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,"//*[@type='submit']"))).click()
+loginkite()
 
 #Click on Postions Page
 driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/div[2]/div[1]/a[4]').click()
